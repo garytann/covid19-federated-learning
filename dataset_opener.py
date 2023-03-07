2873b43d3b1f74041ebbc28c489cbc49a75ae7dc
garytanjy <tgary@uuzu.com>
2023-03-07 16:44:48 +0800

    for data registering

--- dataset_opener.py
+++ dataset_opener.py
@@ -2,6 +2,7 @@
 import pathlib
 import numpy as np
 import substratools as tools
+import pdb
 
 
 class DatasetOpener(tools.Opener):
@@ -21,7 +22,7 @@ class DatasetOpener(tools.Opener):
         p = pathlib.Path(folders[0])
         images_data_path = p / list(p.glob("*_images.npy"))[0]
         labels_data_path = p / list(p.glob("*_labels.npy"))[0]
-
+        
         # load data
         data = {
             "images": np.load(images_data_path),
